a9d030e0c695a18e4e2862d0bd36c86abf238c26
Vianney <vianneyjchin@gmail.com>
2019-11-19 15:44:50 -0800

    change text to input on click

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 
 const TodoList = props => {
+  const [edit, setEdit] = useState(false)
+  const renderTodo = txt => {
+    if (edit) {
+      return <input value={txt} />
+    } else {
+      return <p>{txt}</p>
+    }
+  }
   return (
     <>
       <ul>
         {props.todos.map((todo, index) => (
           <li key={index}>
-            {todo}
-            <button>Edit</button>
+            {renderTodo(todo)}
+            <button onClick={() => setEdit(true)}>Edit</button>
             <button
               onClick={() => {
                 props.deleteTodo(index)
