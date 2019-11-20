7e9e78f02613a2f898a5d2df199772d2c47794f1
Vianney <vianneyjchin@gmail.com>
2019-11-19 21:18:43 -0800

    Separate styles

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,11 @@
 import React, { useState } from 'react'
 import { StyledButton } from '../styles/Button'
-import styled from 'styled-components'
+import { TextContent } from '../styles/TodoList'
+import { ListItem } from '../styles/TodoItem'
 import TextField from '@material-ui/core/TextField'
 import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import NativeSelect from '@material-ui/core/NativeSelect'
-
-const ListItem = styled.li`
-  margin-bottom: 20px;
-  width: 500px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  padding: 2.5%;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 0.5rem;
-`
 
 const TodoList = ({ todos, deleteTodo, reRender }) => {
   return (
@@ -40,6 +30,7 @@ const TodoItem = ({ text }) => {
   const [edit, setEdit] = useState(false)
   const [textValue, setTextValue] = useState(text[0])
   const [priorityValue, setPriorityValue] = useState(text[1])
+
   const renderTodo = () => {
     if (edit) {
       return (
@@ -72,15 +63,14 @@ const TodoItem = ({ text }) => {
       )
     } else {
       return (
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <p style={{ fontSize: '2em', fontWeight: '300' }}>{textValue}</p>
-          <p style={{ fontSize: '2em', fontWeight: '300' }}>
-            {priorityValue} priority
-          </p>
-        </div>
+        <TextContent>
+          <p>{textValue}</p>
+          <p>{priorityValue} priority</p>
+        </TextContent>
       )
     }
   }
+
   const renderButton = () => {
     if (edit) {
       return <StyledButton onClick={() => setEdit(false)}>cancel</StyledButton>
@@ -88,14 +78,17 @@ const TodoItem = ({ text }) => {
       return <StyledButton onClick={() => setEdit(true)}>edit</StyledButton>
     }
   }
+
   const handleChange = e => {
     setTextValue(e.target.value)
   }
+
   const handleSubmit = e => {
     e.preventDefault()
     setTextValue(textValue)
     setEdit(false)
   }
+
   const onPriorityChange = e => {
     setPriorityValue(e.target.value)
   }
---
+++ src/styles/TodoList.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const TextContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  > p {
+    font-size: 2em;
+    font-weight: 300;
+  }
+`
